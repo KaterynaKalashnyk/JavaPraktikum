dc7621641a5a576e32d56c59846718154e646422
KaterynaKalashnyk <122403309+KaterynaKalashnyk@users.noreply.github.com>
2023-05-30 22:08:07 +0200

    Add files via upload

---
+++ lesson1/Light.java
@@ -0,0 +1,22 @@
+package lesson1;
+
+public class Light extends Devices{
+    public Light(boolean isItWorking) {
+        super(isItWorking);
+    }
+    public String AtHome(boolean PersonAtHome){
+        if (true){
+            boolean IsItWorking = true;
+            return "Свет включен";
+        }else{
+            return "Свет выключен";
+        }
+    }
+    public String Status(boolean IsItWorking){
+        if (true){
+            return "Свет работает";
+        }else{
+            return "Свет не работает";
+        }
+    }
+}
